0b972ba0ae7bdcdb1f0845d3749a9a0656d329f3
David Mora <David.mora@codeimmersives.com>
2019-04-25 11:43:56 -0400

    made changes to HTML so upon hitting enter user can add to the list more naturally. created a function that loads data upon onload. made it so user cant add an empty string to the list

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 window.onload = init;
 
 function init() {
+    getTodos();
     document.querySelector('#get').addEventListener('click', getTodos);
     document.querySelector('#post').addEventListener('click', postTodo);
     document.querySelector('#put').addEventListener('click', updateThirdTodo);
 }
 
 function getTodos(event) {
-    event.preventDefault();
+    // event.preventDefault();
 
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://localhost:3000/todos');
@@ -18,19 +19,23 @@ function getTodos(event) {
 
 function postTodo(event) {
     event.preventDefault();
-
-    const newTodo = {
-        text: "Profit",
-        completed: false
-    };
-    
-    const jsonnedTodo = JSON.stringify(newTodo);
-    
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/todos');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = handleData;
-    xhr.send(jsonnedTodo);
+    const userInput = document.querySelector('#user-input').value;
+    if(userInput !== "") {
+        const newTodo = {
+            text: userInput,
+            completed: false
+        };
+        const jsonnedTodo = JSON.stringify(newTodo);
+        
+        
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:3000/todos');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = handleData;
+        xhr.send(jsonnedTodo);
+        
+        document.querySelector('#user-input').value = '';
+    }
 }
 
 function updateThirdTodo(event) {
@@ -49,6 +54,22 @@ function updateThirdTodo(event) {
 
 function handleData(event) {
     const rawData = JSON.parse(event.target.responseText);
-    const {text} = rawData;
-    
+    addToList(rawData);
+    console.log(rawData)
+}
+
+function addToList(rawData) {
+    const addingToUl = document.querySelector('#list-items');
+    if(addingToUl.childElementCount < rawData.length) {
+        rawData.forEach((text) => {
+            const newLi = document.createElement('li');
+            newLi.innerText = text.text;
+            addingToUl.appendChild(newLi);
+        });
+    } else {
+        const newLi = document.createElement('li');
+        console.log(rawData.text)
+        newLi.innerText = rawData.text;
+        addingToUl.appendChild(newLi);
+    }
 }
